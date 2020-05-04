c4faccf80af7de83aef9d25d9714e6ff3f3964ce
adamsoutar <adam@overflo.me>
2020-05-04 10:54:14 +0100

    Refactor Variables

--- src/interpretting/interpreter.rs
+++ src/interpretting/interpreter.rs
@@ -2,6 +2,8 @@ use std::collections::HashMap;
 use crate::parsing::parser;
 use crate::parsing::ast_utils::*;
 use crate::interpretting::interpreter_utils::*;
+use crate::interpretting::variables::Variables;
+use crate::interpretting::variables;
 
 /*
     TODO: Instead of panicking, throw exceptions within the
@@ -10,50 +12,22 @@ use crate::interpretting::interpreter_utils::*;
 
 pub struct Interpreter {
     pub ast: Vec<ASTNode>,
-    pub alloced_values: Vec<AllocedValue>,
-    pub scopestack: Vec<HashMap<String, usize>>
-}
-
-fn print_allocced (av: &Vec<AllocedValue>) {
-    for i in 0..av.len() {
-        let v = &av[i];
-        println!("{} - {}", i,
-            match &v.value {
-                KaffeeValue::Number(n) => {
-                    format!("Number: {}", n)
-                },
-                KaffeeValue::String(s) => {
-                    format!("String: \"{}\"", s)
-                },
-                _ => String::from("TODO: This value type")
-            }
-        )
-    }
-}
-
-fn print_scopestack (stack: &Vec<HashMap<String, usize>>) {
-    for i in 0..stack.len() {
-        let scope = &stack[i];
-        println!("Stack frame {}:", i);
-        for (ident, idx) in scope {
-            println!(" - \"{}\" - {}", ident, idx);
-        }
-    }
+    pub vars: Variables
 }
 
 impl Interpreter {
     pub fn run (&mut self) {
         // TODO: Hoisting pass which puts functions etc.
         //       in the scopestack
-        self.new_scope();
+        self.vars.new_scope();
         for i in 0..self.ast.len() {
             let node = self.ast[i].clone();
             self.eval_node(&node)
         }
 
         println!("\nAllocced values:");
-        print_allocced(&self.alloced_values);
-        print_scopestack(&self.scopestack);
+        self.vars.print_allocced();
+        self.vars.print_scopestack();
     }
 
     fn eval_node (&mut self, node: &ASTNode) {
@@ -74,34 +48,22 @@ impl Interpreter {
     fn assign_variable (&mut self, bin: &BinaryProperties) {
         // TODO: Assignment to non-identifiers
         if let ASTNode::Identifier(id) = bin.left.as_ref() {
-            let idx = self.find_variable_index(id);
-            if self.alloced_values[idx].constant {
+            let idx = self.vars.find_variable_index(id);
+            if self.vars.alloced[idx].constant {
                 panic!("Assignment to constant value \"{}\"", id);
             }
 
             let val = self.resolve_value(bin.right.as_ref());
-            self.alloced_values[idx].value = val;
+            self.vars.alloced[idx].value = val;
         } else {
             panic!("Assignment to non-identifiers is not yet supported")
         }
     }
 
-    fn find_variable_index (&mut self, name: &String) -> usize {
-        let max = self.scopestack.len() - 1;
-        for i in (0..=max).rev() {
-            let hm = &self.scopestack[i];
-            match hm.get(name) {
-                Some(idx) => return idx.clone(),
-                None => continue
-            }
-        }
-        panic!("Unresolved identifier \"{}\"", name)
-    }
-
-    fn define_variable (&mut self, dcl: &DeclarationProperties) {
+    pub fn define_variable (&mut self, dcl: &DeclarationProperties) {
         if let ASTNode::Identifier(id) = dcl.assignment.left.as_ref() {
             let val = self.resolve_value(dcl.assignment.right.as_ref());
-            self.alloc_in_scope(id, val, dcl.constant)
+            self.vars.alloc_in_scope(id, val, dcl.constant)
         } else {
             panic!("Left side of a declaration isn't an identifier")
         }
@@ -115,28 +77,6 @@ impl Interpreter {
         }
     }
 
-    fn new_scope (&mut self) {
-        self.scopestack.push(HashMap::new())
-    }
-
-    fn alloc_in_scope (&mut self, identifier: &String, value: KaffeeValue, constant: bool) {
-        let idx = self.alloced_values.len();
-        self.alloc_value(value, constant);
-        self.add_to_scope(identifier.clone(), idx);
-    }
-
-    fn add_to_scope (&mut self, identifier: String, alloc_index: usize) {
-        let idx = self.scopestack.len() - 1;
-        self.scopestack[idx].insert(identifier, alloc_index);
-    }
-
-    fn alloc_value (&mut self, value: KaffeeValue, constant: bool) {
-        self.alloced_values.push(AllocedValue {
-            value,
-            constant,
-            ref_count: 0
-        })
-    }
 }
 
 pub fn new (code: String) -> Interpreter {
@@ -145,7 +85,6 @@ pub fn new (code: String) -> Interpreter {
 
     Interpreter {
         ast,
-        alloced_values: vec![],
-        scopestack: vec![]
+        vars: variables::new()
     }
 }
---
+++ src/interpretting/variables.rs
@@ -0,0 +1,79 @@
+use std::collections::HashMap;
+use crate::interpretting::interpreter_utils::*;
+use crate::parsing::ast_utils::*;
+
+pub struct Variables {
+    pub alloced: Vec<AllocedValue>,
+    pub scopestack: Vec<HashMap<String, usize>>
+}
+
+impl Variables {
+    pub fn find_variable_index (&mut self, name: &String) -> usize {
+        let max = self.scopestack.len() - 1;
+        for i in (0..=max).rev() {
+            let hm = &self.scopestack[i];
+            match hm.get(name) {
+                Some(idx) => return idx.clone(),
+                None => continue
+            }
+        }
+        panic!("Unresolved identifier \"{}\"", name)
+    }
+
+    pub fn new_scope (&mut self) {
+        self.scopestack.push(HashMap::new())
+    }
+
+    pub fn alloc_in_scope (&mut self, identifier: &String, value: KaffeeValue, constant: bool) {
+        let idx = self.alloced.len();
+        self.alloc_value(value, constant);
+        self.add_to_scope(identifier.clone(), idx);
+    }
+
+    pub fn add_to_scope (&mut self, identifier: String, alloc_index: usize) {
+        let idx = self.scopestack.len() - 1;
+        self.scopestack[idx].insert(identifier, alloc_index);
+    }
+
+    pub fn alloc_value (&mut self, value: KaffeeValue, constant: bool) {
+        self.alloced.push(AllocedValue {
+            value,
+            constant,
+            ref_count: 0
+        })
+    }
+
+    pub fn print_allocced (&self) {
+        for i in 0..self.alloced.len() {
+            let v = &self.alloced[i];
+            println!("{} - {}", i,
+                match &v.value {
+                    KaffeeValue::Number(n) => {
+                        format!("Number: {}", n)
+                    },
+                    KaffeeValue::String(s) => {
+                        format!("String: \"{}\"", s)
+                    },
+                    _ => String::from("TODO: This value type")
+                }
+            )
+        }
+    }
+
+    pub fn print_scopestack (&self) {
+        for i in 0..self.scopestack.len() {
+            let scope = &self.scopestack[i];
+            println!("Stack frame {}:", i);
+            for (ident, idx) in scope {
+                println!(" - \"{}\" - {}", ident, idx);
+            }
+        }
+    }
+}
+
+pub fn new () -> Variables {
+    Variables {
+        alloced: vec![],
+        scopestack: vec![]
+    }
+}
