d367884a7f5500a750c8eb932e5c92d1d92e67ef
Adam Soutar <adam@overflo.me>
2020-05-06 16:28:10 +0100

    Stringify std_lib function

--- src/main.rs
+++ src/main.rs
@@ -7,16 +7,11 @@ use parsing::printer::print_ast;
 fn main() {
     let code = String::from("
         let dude = {
-            name: \"Adam\"
+            name: \"Adam\",
+            age: 17
         }
 
-        let key = \"age\"
-        dude[key] = 17
-        dude.project = \"KaffeeLang\"
-
         println(dude.name)
-        println(dude[\"age\"])
-        println(dude.project)
     ");
     let mut interp = interpreter::new(code);
     print_ast(&interp.ast);
--- src/std_lib/functions.rs
+++ src/std_lib/functions.rs
@@ -1,19 +1,24 @@
 use crate::interpretting::interpreter_utils::*;
 
-fn rust_stringify (value: &KaffeeValue) -> String {
+// TODO: To be able to stringify objects, we need access to the alloc array
+fn rust_stringify (value: &KaffeeValue, obj_depth: i32) -> String {
     match value {
         KaffeeValue::Number(n) => format!("{}", n),
         KaffeeValue::String(st) => st.clone(),
         KaffeeValue::Boolean(bl) => format!("{}", bl),
         KaffeeValue::Null => String::from("Kaffee::null"),
         // TODO: Exception for this
-        _ => String::from("Unstringifyable value")
+        _ => panic!("Unstringifyable value")
     }
 }
 
 fn native_println (args: Vec<KaffeeValue>) -> KaffeeValue {
-    println!("{}", rust_stringify(&args[0]));
+    println!("{}", rust_stringify(&args[0], 0));
     KaffeeValue::Null
+}
+
+fn native_stringify(args: Vec<KaffeeValue>) -> KaffeeValue {
+    KaffeeValue::String(rust_stringify(&args[0], 0))
 }
 
 pub fn get_std_lib_mappings () -> Vec<NativeMapping> {
@@ -22,6 +27,11 @@ pub fn get_std_lib_mappings () -> Vec<NativeMapping> {
             name: String::from("println"),
             arg_count: 1,
             func: native_println
+        },
+        NativeMapping {
+            name: String::from("stringify"),
+            arg_count: 1,
+            func: native_stringify
         }
     ]
 }
